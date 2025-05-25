0e278c48f15cd8358cced57ff789e9135805e059
Mark <mark65054@gmail.com>
2025-05-25 14:56:11 +0800

    sim direct

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import copy,os
 import numpy as np
 from scipy import ndimage
 from tqdm import trange
+from itertools import product
 
 LabelBase.register(name='chinese', fn_regular='C:\\Windows\\Fonts\\msjh.ttc')
 
@@ -29,6 +30,60 @@ class header_info:
         self.bending_loss = int(_input_text.split('\n')[3].split(' ')[2])
         self.num_net = int(_input_text.split('\n')[4].split(' ')[2])
         self.net_data = [net_info(_x) for _x in _input_text.split('\n')[5:] if _x!=""]
+
+class method:
+    def __init__(self , _data:header_info):
+        self.data = _data
+        self.cross_list = [[] for _ in range(len(self.data.net_data))]
+
+    def find_crossing(self):
+        self.cross_list = [[] for _ in range(len(self.data.net_data))]
+        for _a in range(0 , len(self.data.net_data)):
+            for _b in range(_a+1 ,len(self.data.net_data)):
+                _cross_coord = method.cross_at(self.data.net_data[_a] , self.data.net_data[_b])
+                self.cross_list[_a] += _cross_coord
+                self.cross_list[_b] += _cross_coord
+        return None
+    def cross_at(_pina_data , _pinb_data):
+        _neta_area = set([tuple(p) for p in _pina_data.line_coordinate] +  [tuple(p) for p in _pina_data.pin_coordinate])
+        _netb_area = set([tuple(p) for p in _pinb_data.line_coordinate] +  [tuple(p) for p in _pinb_data.pin_coordinate])
+        _output = list(_neta_area & _netb_area)
+        return _output
+    def direct_connection(_pin_location):
+        output = [[],[]] #|^ and _|
+        _LD_point = np.array([min(_pin_location.T[0]) , min(_pin_location.T[1])])
+        _RT_point = np.array([max(_pin_location.T[0]) , max(_pin_location.T[1])])
+        _Dhor = [[_x , _LD_point[1]] for _x in range(_LD_point[0] + 1, _RT_point[0])]
+        _Rver = [[_RT_point[0] , _y] for _y in range(_LD_point[1] + 1 , _RT_point[1])]
+        _Thor = [[_x , _RT_point[1]] for _x in range(_LD_point[0] + 1, _RT_point[0])]
+        _Lver = [[_LD_point[0] , _y] for _y in range(_LD_point[1] + 1 , _RT_point[1])]
+        _LT_point = np.array([_LD_point[0] , _RT_point[1]])
+        _RD_point = np.array([_RT_point[0] , _LD_point[1]])
+        if _pin_location[0][0] > _pin_location[1][0]:
+            _Dhor.reverse()
+            _Thor.reverse()
+        if _pin_location[0][1] > _pin_location[1][1]:
+            _Rver.reverse()
+            _Lver.reverse()
+
+        if len(_Dhor) == 0 or len(_Rver) == 0:
+            output[0] = _Lver + _Thor
+            output[1] = _Dhor + _Rver
+        else:
+            if np.linalg.norm(_LD_point - _pin_location[0]) == 0:
+                output[0] = _Lver + [_LT_point.tolist()] + _Thor
+                output[1] = _Dhor + [_RD_point.tolist()] + _Rver
+            elif np.linalg.norm(_LT_point - _pin_location[0]) == 0:
+                output[0] = _Lver + [_LD_point.tolist()] + _Dhor
+                output[1] = _Thor + [_RT_point.tolist()] + _Rver
+            elif np.linalg.norm(_RD_point - _pin_location[0]) == 0:
+                output[0] = _Rver + [_RT_point.tolist()] + _Thor
+                output[1] = _Dhor + [_LD_point.tolist()] + _Lver
+            elif np.linalg.norm(_RT_point - _pin_location[0]) == 0:
+                output[0] = _Rver + [_RD_point.tolist()] + _Dhor
+                output[1] = _Thor + [_LT_point.tolist()] + _Lver
+
+        return output
 
 class PIC:
     def __init__(self,input_file:str):
@@ -66,10 +121,7 @@ class PIC:
                     for _pathb in _cmp_pin.line_strip:
                         _eq_start = _patha[0][0] == _pathb[0][0] and _patha[0][1] == _pathb[0][1]
                         _eq_end = _patha[1][0] == _pathb[1][0] and _patha[1][1] == _pathb[1][1]
-                        if _eq_start and _eq_end:#overlap
-                            _output += 1
-                            pass
-                        elif _eq_end:#corss
+                        if _eq_end:#corss
                             _output += 1
         return _output
     def loss_cal_bend(self):
@@ -96,42 +148,113 @@ class PIC:
             _output += len(_x.line_strip)
         return _output
     def loss(self):
+        self.convert_toline()
         _wire_length = self.loss_cal_length()
         _cross = self.loss_cal_cross()
         _bend = self.loss_cal_bend()
         _total_loss = _wire_length * self.data.propagation_loss + _cross * self.data.crossing_loss + _bend * self.data.bending_loss
         self.data_pack.append([_total_loss,_wire_length,_cross,_bend])
         return None
-    def direct_connect(self):
+    def convert_toline(self):
         for _pin in self.data.net_data:
             _pin.connected = False
-            _pin.line_coordinate = []
             _pin.line_strip = []
-            _current_coordinate = np.array([min(_pin.pin_coordinate.T[0]),min(_pin.pin_coordinate.T[1])])
-            for _wire_x in range(min(_pin.pin_coordinate.T[0]),max(_pin.pin_coordinate.T[0])+1):
-                _wire_y = min(_pin.pin_coordinate.T[1])
-                _next_coordinate = np.array([_wire_x,_wire_y])
-                _eq_pin0 = _next_coordinate[0] == _pin.pin_coordinate[0][0] and _next_coordinate[1] == _pin.pin_coordinate[0][1]
-                _eq_pin1 = _next_coordinate[0] == _pin.pin_coordinate[1][0] and _next_coordinate[1] == _pin.pin_coordinate[1][1]
-                if  not(_eq_pin0 or _eq_pin1):
-                    _pin.line_coordinate.append(_next_coordinate)
-                if not(_next_coordinate[0] == _current_coordinate[0] and _next_coordinate[1] == _current_coordinate[1]):
-                    _pin.line_strip.append(np.array([_current_coordinate,_next_coordinate]))
-                _current_coordinate = _next_coordinate
-            for _wire_y in range(min(_pin.pin_coordinate.T[1]),max(_pin.pin_coordinate.T[1])+1):
-                _wire_x = max(_pin.pin_coordinate.T[0])
-                _next_coordinate = np.array([_wire_x,_wire_y])
-                _eq_pin0 = _next_coordinate[0] == _pin.pin_coordinate[0][0] and _next_coordinate[1] == _pin.pin_coordinate[0][1]
-                _eq_pin1 = _next_coordinate[0] == _pin.pin_coordinate[1][0] and _next_coordinate[1] == _pin.pin_coordinate[1][1]
-                if  not(_eq_pin0 or _eq_pin1):
-                    _pin.line_coordinate.append(_next_coordinate)
-                if not(_next_coordinate[0] == _current_coordinate[0] and _next_coordinate[1] == _current_coordinate[1]):
-                    _pin.line_strip.append(np.array([_current_coordinate,_next_coordinate]))
+            _current_coordinate = _pin.pin_coordinate[0]#start assume
+            _end_coordinate = _pin.pin_coordinate[1]#end
+            _connect_in = _pin.line_coordinate.copy()
+            _x_isclose = _current_coordinate[0] -1 <= _connect_in[0][0] and _connect_in[0][0] <= _current_coordinate[0] +1 
+            _y_isclose = _current_coordinate[1] -1 <= _connect_in[0][1] and _connect_in[0][1] <= _current_coordinate[1] +1 
+            if not (_x_isclose and _y_isclose):
+                _current_coordinate = _pin.pin_coordinate[1]#start change
+                _end_coordinate = _pin.pin_coordinate[0]#end
+            _connect_in.append(_end_coordinate)
+            for _next_coordinate in _connect_in:
+                _line = np.array([_current_coordinate,_next_coordinate])
+                _pin.line_strip.append(_line)
                 _current_coordinate = _next_coordinate
             _pin.connected = True
         return None
+    def direct_connect(self):
+        if len(self.data.net_data) < 25:
+            self.data.net_data = PIC.best_direct_connect_in_list(self.data.net_data)
+        else:
+            _data_buffer = PIC.from_smallest_direct_connect_in_list(self.data.net_data)
+            _crossing_table = [0 for _ in range(len(_data_buffer))]
+            for _a in range(0,len(_data_buffer)):
+                for _b in range(_a +1,len(_data_buffer)):
+                    _crossing_count = len(method.cross_at(_data_buffer[_a] , _data_buffer[_b]))
+                    _crossing_table[_a] += _crossing_count
+                    _crossing_table[_b] += _crossing_count
+            _reroute_list = []
+            _noroute_list = []
+            for _it in range(len(_crossing_table)):
+                if _crossing_table[_it] == 0:
+                    _noroute_list.append(_data_buffer[_it])
+                else:
+                    _reroute_list.append(_data_buffer[_it])
+            _finish_reroute = PIC.best_direct_connect_in_list(_reroute_list)
+            self.data.net_data = _finish_reroute + _noroute_list
+        self.matrix = self.convert_tomatrix()
+        self.all_matrix.append(self.matrix)
+        return None
+
+    @staticmethod
+    def best_direct_connect_in_list(_pin_list:list[net_info])->list[net_info]:
+        _output = copy.deepcopy(_pin_list)
+        all_options = [method.direct_connection(net.pin_coordinate) for net in _pin_list]
+        all_selectors = list(product([0, 1], repeat=len(all_options)))
+    
+        best_cross_count = float('inf')
+        best_routing = None
+
+        for selector in all_selectors:
+            for i, route_idx in enumerate(selector):
+                _pin_list[i].line_coordinate = all_options[i][route_idx]
+        
+            total_cross = 0
+            for i in range(len(_pin_list)):
+                total_cross += sum(
+                    len(method.cross_at(_pin_list[i], _pin_list[j]))
+                    for j in range(i+1, len(_pin_list))
+                )
+
+            if total_cross < best_cross_count:
+                best_cross_count = total_cross
+                best_routing = [net.line_coordinate.copy() for net in _pin_list]
+
+        # 最佳路徑寫回
+        for i in range(len(_pin_list)):
+            _output[i].line_coordinate = best_routing[i]
+        return _output
+
+    @staticmethod
+    def from_smallest_direct_connect_in_list(pin_list: list[net_info]) -> list[net_info]:
+        output = []
+        distances = [np.linalg.norm(net.pin_coordinate[0] - net.pin_coordinate[1]) for net in pin_list]
+        sorted_indices = sorted(range(len(distances)), key=lambda i: distances[i])
+
+        for idx in sorted_indices:
+            original_net = pin_list[idx]
+            route_options = method.direct_connection(original_net.pin_coordinate)
+            cross_counts = []
+
+            for option in route_options:
+                test_net = copy.deepcopy(original_net)
+                test_net.line_coordinate = option
+                cross_count = sum(len(method.cross_at(test_net, placed)) for placed in output)
+                cross_counts.append(cross_count)
+
+            best_route = route_options[np.argmin(cross_counts)]
+            selected_net = copy.deepcopy(original_net)
+            selected_net.line_coordinate = best_route
+            output.append(selected_net)
+
+        return output
+
             
     def forward(self):
+        _method = method(self.data)
+        _method.find_crossing()
         self.matrix = self.convert_tomatrix()
         self.all_matrix.append(self.matrix)
         return None
@@ -306,8 +429,6 @@ class MatrixIterationVisualize(App):
         self.matrix = np.random.randint(0, 100, size=(self.col, self.row))
         self.__refresh_matrix()
         return None
-
-
 
 if __name__ == '__main__':
     _project = PIC("pic5x5.in")
